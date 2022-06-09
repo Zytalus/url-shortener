a599edf9d24acac5deeb0a283441d5a7545273c3
Nicholas Deckard <nicholas.deckard@crazymagic.studio>
2022-06-08 21:17:52 -0500

    add UrlShortenerStack

---
+++ lambda/handler.py
@@ -0,0 +1,70 @@
+import json
+import logging
+import os
+import uuid
+import boto3
+
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+
+
+def main(event, context):
+    LOG.info("EVENT: " + json.dumps(event))
+
+    query_string_params = event["queryStringParameters"]
+    if query_string_params is not None:
+        target_url = query_string_params['targetUrl']
+        if target_url is not None:
+            return create_short_url(event)
+    path_parameters = event['pathParameters']
+    if path_parameters is not None:
+        if path_parameters['proxy'] is not None:
+            return read_short_url(event)
+
+    return {
+        'statusCode': 200,
+        'body': 'usage: https://go.crazymagic.studio/?targetUrl=URL-GOES-HERE'
+    }
+
+
+def create_short_url(event):
+    table_name = os.environ.get('TABLE_NAME')
+    target_url = event["queryStringParameters"]['targetUrl']
+    m_id = str(uuid.uuid4())[0:8]
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    table.put_item(Item={
+        'id': m_id,
+        'target_url': target_url
+    })
+    url = "https://" \
+        + event["requestContext"]["domainName"] \
+        + event["requestContext"]["path"] \
+        + m_id
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': "Created URL: %s" % url
+    }
+
+
+def read_short_url(event):
+    m_id = event['pathParameters']['proxy']
+    table_name = os.environ.get('TABLE_NAME')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    response = table.get_item(Key={'id': m_id})
+    LOG.debug("RESPONSE: " + json.dumps(response))
+
+    item = response.get('Item', None)
+    if item is None:
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'text/plain'},
+            'body': 'No redirect found for: ' + m_id
+        }
+    return {
+        'statusCode': 301,
+        'headers': {'Location': item.get('target_url')}
+    }
--- url_shortener/url_shortener_stack.py
+++ url_shortener/url_shortener_stack.py
@@ -1,19 +1,35 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_lambda,
+    aws_apigateway,
+    aws_certificatemanager as acm,
+    aws_route53,
+    aws_route53_targets
 )
 from constructs import Construct
+
 
 class UrlShortenerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UrlShortenerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        table = dynamodb.Table(self, "mapping-table", partition_key=dynamodb.Attribute(
+            name="id", type=dynamodb.AttributeType.STRING))
+        function = aws_lambda.Function(self, "backend",
+                                       runtime=aws_lambda.Runtime.PYTHON_3_7,
+                                       handler="handler.main",
+                                       code=aws_lambda.Code.from_asset("./lambda"))
+        table.grant_read_write_data(function)
+        function.add_environment("TABLE_NAME", table.table_name)
+        api = aws_apigateway.LambdaRestApi(self, 'api',
+                                           handler=function,
+                                           domain_name=aws_apigateway.DomainNameOptions(
+                                               domain_name="go.crazymagic.studio",
+                                               certificate=acm.Certificate.from_certificate_arn(self, 'cert', "arn:aws:acm:us-east-1:478062622781:certificate/3f63a892-ae7b-41f7-aba4-f32c64a42f6e")
+                                           ))
+        hosted_zone = aws_route53.HostedZone.from_hosted_zone_attributes(self, 'imported-hosted-zone',
+                                                                         hosted_zone_id="Z03983981EGB0ECNSGVGV",
+                                                                         zone_name="go.crazymagic.studio")
+        route = aws_route53.ARecord(self, 'go-alias-record', zone=hosted_zone, target=aws_route53.RecordTarget.from_alias(aws_route53_targets.ApiGateway(api)))
